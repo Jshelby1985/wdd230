b4692f48f1f2608e28610d9a222e70ed54cf9ac9
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2023-05-10 22:26:09 -0500

    added time stamp to header of commerce

--- chamber/scripts/time_stamp.js
+++ chamber/scripts/time_stamp.js
@@ -1,3 +1,5 @@
 const year = new Date().getFullYear();
+const date = new Date().toLocaleDateString();
 document.getElementById("lastUpdate").textContent = document.lastModified;
 document.getElementById("year").textContent = year;
+document.getElementById("current").textContent = date;
