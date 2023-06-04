e4178c1be1e48c06d11433aca37b90d18e277ec9
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2023-06-03 23:24:14 -0500

    fixed js banner and much css

--- chamber/scripts/time_stamp.js
+++ chamber/scripts/time_stamp.js
@@ -1,5 +1,20 @@
-const year = new Date().getFullYear();
-const date = new Date().toLocaleDateString();
+const date = new Date();
+const year = date.getFullYear();
+const stringDate = date.toLocaleDateString();
 document.getElementById("lastUpdate").textContent = document.lastModified;
 document.getElementById("year").textContent = year;
-document.getElementById("current").textContent = date;
+document.getElementById("current").textContent = stringDate;
+
+//Banner
+let banner = document.getElementById("banner");
+createBanner();
+function createBanner(){
+    let currentDay = date.getDay();
+    //let currentDay = 1;
+
+    if (currentDay === 2 || currentDay === 1){
+        banner.style.display = "block";
+    } else {
+        banner.style.display = "none";
+    };
+};
