80a70c7f42a509900d9a621f9b1ada45f2db356d
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2023-06-14 00:22:09 -0500

    built Lazy load

---
+++ lesson7/scripts/lazy.js
@@ -0,0 +1,22 @@
+let options = {
+    threshold: 0.8
+}
+
+const observer = new IntersectionObserver(imageObserver, options);
+
+function imageObserver(entries, observer){
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            const img = entry.target;
+            const img_src = img.dataset.src;
+            img.src = img_src;
+        }
+    })
+}
+
+let images = document.querySelectorAll("img[data-src]");
+
+images.forEach((img) => {
+    observer.observe(img);
+});
+
