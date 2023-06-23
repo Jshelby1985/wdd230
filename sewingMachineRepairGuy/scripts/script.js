656d4cf78fbbe4e7bd7775782bd449c04c641129
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2023-06-23 02:10:17 -0500

    Added weather to sewing home page

--- sewingMachineRepairGuy/scripts/script.js
+++ sewingMachineRepairGuy/scripts/script.js
@@ -59,3 +59,42 @@ let images = document.querySelectorAll("img[data-src]");
 images.forEach((img) => {
   observer.observe(img)
 })
+
+//Weather Api //  
+  const currentTemp = document.querySelector('#current-temp');
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('figcaption');
+  const windSpeed = document.querySelector('#current-speed');
+  const WindChill = document.querySelector('#chill');
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Moberly&units=imperial&APPID=b840405b3f4959aafe6b1fb2a87611a3"
+  
+  
+  async function apiFetch() {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data); // this is for testing the call
+          displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+      } catch (error) {
+          console.log(error);
+      }
+    }
+    
+    apiFetch();
+  
+    function displayResults(weatherData) {
+      currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+      windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
+      WindChill.innerHTML = `<strong>${weatherData.main.feels_like.toFixed(0)}</strong>`;
+  
+      const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+      const desc = weatherData.weather[0].description;
+  
+      weatherIcon.setAttribute('src', iconsrc);
+      weatherIcon.setAttribute('alt', desc);
+      captionDesc.textContent = desc;
+    }
