3436536f55276dc6ba8c1cad1e61c5ec01abd321
Jshelby1985 <48341895+Jshelby1985@users.noreply.github.com>
2023-07-11 01:22:47 -0500

    Bountiful Home page work

--- bountifulFoods/scripts/script.js
+++ bountifulFoods/scripts/script.js
@@ -1,4 +1,34 @@
+//Time Stamp//
 const stamp = new Date();
 const year = stamp.getFullYear();
 document.getElementById("lastUpdate").textContent = document.lastModified;
 document.getElementById("year").textContent = year;
+
+
+
+
+//Burger Menu//
+
+function toggleMenu(){
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+
+
+
+//Slideshow//
+const slideshowImages = document.querySelectorAll(".slideshow .slideshow-img");
+const nextImageDelay = 2500;
+let currentImageCounter = 0;
+
+slideshowImages[currentImageCounter].style.display = "block";
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+  slideshowImages[currentImageCounter].style.display = "none";
+  currentImageCounter = (currentImageCounter +1) % slideshowImages.length;
+  slideshowImages[currentImageCounter].style.display = "block";
+
+};
+
